75c0eaa1590da5edd4a09402668d5175d7e7de08
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2022-12-05 16:53:32 +0100

    add publicphone.php
    :)

---
+++ Script/publicphone.php
@@ -0,0 +1,39 @@
+<?php
+class Caller {
+    private Int $number;
+    Private Float $moneyAmount;
+    Private Phonebooth phonebooth;
+    Public AddMoney() : void
+    {
+
+    }
+    Public StoreMoney() : string
+    {
+
+    }
+    Public Use() : void
+    {
+
+    }
+
+}
+class Phonebooth {
+    Private float price;
+    Private Reciever reciver;
+    Public Call() : bool
+    {
+    }
+    Public ReturnMoney() : float
+    {
+
+    }
+}
+
+class Reciever {
+    private Int number;
+    Private Phonebooth phonebooth[]; 
+    Public HangUp() : void
+    {
+
+    }
+}
