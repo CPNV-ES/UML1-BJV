9017c12a6e46e538ebbac36b5d10c9a1f6eeb084
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-19 15:10:18 +0100

    Review

--- Script/publicphone.php
+++ Script/publicphone.php
@@ -1,41 +1,45 @@
 <?php
 
 class Caller {
-    private Int $number;
+    private string $number;
     private Float $moneyAmount;
     private Phonebooth $phonebooth;
     public function AddMoney() : void
     {
 
     }
-    public function StoreMoney() : string
+    public function StoreMoney() : float
     {
 
     }
-    public function Use() : void
+    public function Use($phonebooth) : void
     {
-
+        $this.phonebooth = $phonebooth;
     }
 
 }
 
 class Phonebooth {
     private float $price;
-    private Reciever $reciver;
+    private Receiver $receiver;//juste add phone number to call
     public function Call() : bool
     {
     }
     public function ReturnMoney() : float
     {
-
+        
     }
+
+    //TODO Add Method to manage the cost
 }
 
-class Reciever {
-    private Int $number;
+class Receiver {
+    private string $number;
     private Phonebooth $phonebooth; 
     public function HangUp() : void
     {
 
     }
+
+    //TODO inheritance with caller
 }
