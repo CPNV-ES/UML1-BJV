502d5b2cf028167db7347852d0f48e430906cffd
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-19 16:36:04 +0100

    Review

--- Script/main.php
+++ Script/main.php
@@ -5,13 +5,14 @@ require_once('Phonebooth.php');
 require_once('Receiver.php');
 require_once('Caller.php');
 
+//TODO Rewrite this file into a test class. Add assertion after each step to check if object's behavior meet the requirements.
 $caller = new Caller();
 $phonebooth = new Phonebooth();
 
 $caller->AddMoney();
 $caller->Use($phonebooth);
 
-$receiver = $phonebooth->Call("+41 000 000 00 00");
-$phonebooth->RemoveMoney();
+$receiver = $phonebooth->Call("+41 000 000 00 00");////TODO the caller is using Phonebooth and start the dialog... why did you initiate the receiver ?
+$phonebooth->RemoveMoney();//TODO Remove Money should be invoked by HangUpd, no ?
 $receiver->HangUp();
-$phonebooth->ReturnMoney();
+$phonebooth->ReturnMoney();//TODO no return ?
